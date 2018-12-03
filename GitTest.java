38d113623021080fa445771b20c0a5ea7c4dd33c
youssef <44912677hd-youssef@users.noreply.github.com>
2018-12-03 12:38:48 -0500

    features
    added a new method feature and spaced things out

--- GitTest.java
+++ GitTest.java
@@ -1,12 +1,17 @@
 public class GitTest{ //Youssef Haddad
 
-
 	private String declaration;
 
 	public GitTest(){
 		declaration = "This is my GitHub test";
 	}
+
 	public String toString(){
 		return declaration;
 	}
+
+	public void setString(String a){
+		declaration = a;
+	}
+
 }
